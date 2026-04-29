e5ff500f530265915d5a4a7566ecb19b343912c7
Danish Ruzi <danishruzi28@gmail.com>
2026-04-29 23:59:39 +0800

    Adding locations and directions

--- level1.js
+++ level1.js
@@ -1,13 +1,18 @@
 function loadLevel1() {
-    return `
-        <a-marker type="barcode" value="1">
-            <a-text value="LEVEL 1: LAB 1.1" look-at="[camera]" position="0 1 0" align="center"></a-text>
-            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 0 0"></a-entity>
+return `
+        <a-marker type="barcode" value="5">
+            <a-text value="AL-TABARI LAB" look-at="[camera]" position="0 1.2 0" align="center" color="#00FFFF"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 0 0" scale="0.5 0.5 0.5"></a-entity>
         </a-marker>
 
-        <a-marker type="barcode" value="2">
-            <a-text value="LEVEL 1: DEAN OFFICE" look-at="[camera]" position="0 1 0" align="center"></a-text>
-            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 90 0"></a-entity>
+        <a-marker type="barcode" value="6">
+            <a-text value="CAFE / LOUNGE" look-at="[camera]" position="0 1.2 0" align="center" color="#00FFFF"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 90 0" scale="0.5 0.5 0.5"></a-entity>
+        </a-marker>
+
+        <a-marker type="barcode" value="7">
+            <a-text value="MALE MUSALLA" look-at="[camera]" position="0 1.2 0" align="center" color="#00FFFF"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 -90 0" scale="0.5 0.5 0.5"></a-entity>
         </a-marker>
     `;
 }
--- level2.js
+++ level2.js
@@ -1,13 +1,18 @@
 function loadLevel2() {
-    return `
-        <a-marker type="barcode" value="3">
-            <a-text value="LEVEL 2: LAB 2.1" look-at="[camera]" position="0 1 0" align="center"></a-text>
-            <a-entity geometry="primitive: cone" material="color: orange" rotation="-90 0 0"></a-entity>
+return `
+        <a-marker type="barcode" value="8">
+            <a-text value="POSTGRADUATE OFFICE" look-at="[camera]" position="0 1.2 0" align="center" color="#FFD700"></a-text>
+            <a-entity geometry="primitive: cone" material="color: orange" rotation="-90 0 0" scale="0.5 0.5 0.5"></a-entity>
         </a-marker>
 
-        <a-marker type="barcode" value="4">
-            <a-text value="LEVEL 2: MEETING ROOM" look-at="[camera]" position="0 1 0" align="center"></a-text>
-            <a-entity geometry="primitive: cone" material="color: orange" rotation="-90 -90 0"></a-entity>
+        <a-marker type="barcode" value="9">
+            <a-text value="MAIN MEETING ROOM" look-at="[camera]" position="0 1.2 0" align="center" color="#FFD700"></a-text>
+            <a-entity geometry="primitive: cone" material="color: orange" rotation="-90 90 0" scale="0.5 0.5 0.5"></a-entity>
+        </a-marker>
+
+        <a-marker type="barcode" value="10">
+            <a-text value="FEMALE MUSALLA" look-at="[camera]" position="0 1.2 0" align="center" color="#FFD700"></a-text>
+            <a-entity geometry="primitive: cone" material="color: orange" rotation="-90 -90 0" scale="0.5 0.5 0.5"></a-entity>
         </a-marker>
     `;
 }
