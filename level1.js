854c8355917e6d5608c30a4ffe4e24ece839cd5b
Danish Ruzi <danishruzi28@gmail.com>
2026-04-30 01:45:29 +0800

    Adding all locations in Level 1

--- level1.js
+++ level1.js
@@ -1,36 +1,36 @@
 function loadLevel1() {
-let m5Content = ""; // Toilet Marker
-    let m2Content = ""; // Junction Marker
-    let m3Content = ""; // LR6 Marker
+// We will use Barcode 1 as the primary Entrance marker
+    let arrow1 = "-90 0 0"; // Default
+    let dist1 = "0m";
 
-    // --- NAVIGATION LOGIC: TO LR6 ---
-    if (currentTarget === "LR6") {
-        
-        // At Marker 5 (Toilet): Exit and go to junction
-        // Rotation 180 assumes the toilet is 'behind' the main flow
-        m5Content = `
-            <a-text value="EXIT TOILET AREA" position="0 1.5 0" align="center" color="#00FFFF"></a-text>
-            <a-text value="Head to Main Junction (15m)" position="0 1.2 0" align="center" scale="0.8 0.8 0.8"></a-text>
-            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 180 0"></a-entity>`;
-
-        // At Marker 2 (Junction): Turn toward Classrooms
-        // Rotation 90 assumes a Right turn is needed
-        m2Content = `
-            <a-text value="MAIN JUNCTION" position="0 1.5 0" align="center" color="#00FFFF"></a-text>
-            <a-text value="Turn Right for LR6 (20m)" position="0 1.2 0" align="center" scale="0.8 0.8 0.8"></a-text>
-            <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 90 0"></a-entity>`;
-
-        // At Marker 3 (LR6): Arrival!
-        m3Content = `
-            <a-text value="DESTINATION REACHED" position="0 1.8 0" align="center" color="#00FF00" scale="1.2 1.2 1.2"></a-text>
-            <a-text value="LR6" position="0 1.4 0" align="center" color="#FFFFFF"></a-text>
-            <a-entity geometry="primitive: sphere" material="color: gold" 
-                      animation="property: scale; to: 1.2 1.2 1.2; dir: alternate; dur: 1000; loop: true"></a-entity>`;
+    switch(currentTarget) {
+        case 'Mens Toilet 1': 
+            arrow1 = "-90 -90 0"; dist1 = "10m"; break;
+        case 'LR6': 
+            arrow1 = "-90 0 0"; dist1 = "45m"; break;
+        case 'Cafe 1': 
+            arrow1 = "-90 90 0"; dist1 = "20m"; break;
+        case 'Elevator': 
+            arrow1 = "-90 0 0"; dist1 = "30m"; break;
+        // Add more cases here as we set directions...
+        default:
+            arrow1 = "-90 0 0"; dist1 = "Scan to Begin";
     }
 
-    return `
-        <a-marker type="barcode" value="5">${m5Content}</a-marker>
-        <a-marker type="barcode" value="2">${m2Content}</a-marker>
-        <a-marker type="barcode" value="3">${m3Content}</a-marker>
+    // Logic for the Arrival (Target) Markers
+    // This part makes the destination show the 'Arrival Effect' automatically
+    let arrivalEffect = `
+        <a-text value="YOU HAVE ARRIVED" position="0 1.8 0" align="center" color="#00FF00"></a-text>
+        <a-text value="${currentTarget}" position="0 1.4 0" align="center" color="#FFFFFF"></a-text>
+        <a-entity geometry="primitive: sphere" material="color: gold" animation="property: scale; to: 1.2 1.2 1.2; dir: alternate; loop: true"></a-entity>
     `;
+
+    return `
+        <a-marker type="barcode" value="1">
+            <a-text value="CHECKPOINT 1" position="0 1.5 0" align="center" color="#00FFFF"></a-text>
+            <a-text value="To ${currentTarget}: ${dist1}" position="0 1.2 0" align="center" color="#FFD700" scale="0.8 0.8 0.8"></a-text>
+            <a-entity geometry="primitive: cone" material="color: lime" rotation="${arrow1}"></a-entity>
+        </a-marker>
+
+        `;
 }
