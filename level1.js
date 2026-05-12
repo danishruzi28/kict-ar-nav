97925d9ad8f33037e309c866db067e4c117847b2
Danish Ruzi <danishruzi28@gmail.com>
2026-05-12 23:59:37 +0800

    Changing colour

--- level1.js
+++ level1.js
@@ -6,12 +6,12 @@ function loadLevel1() {
     if (currentTarget === "LR 5") {
         // At Lobby (Marker 1), point toward LR 5
         marker1Content =`
-            <a-text value="TURN RIGHT FOR LR 5" position="0 1.5 0" align="center" color="#00FFFF" look-at="[camera]"></a-text>
+            <a-text value="TURN RIGHT FOR LR 5" position="0 1.5 0" align="center" color="#000000" look-at="[camera]"></a-text>
             <a-entity geometry="primitive: cone" material="color: lime" rotation="-90 -90 0"></a-entity>`;
             
         // At LR 5 (Marker 2), Arrival Effect
         marker2Content = `
-            <a-text value="YOU HAVE ARRIVED YOUR DESTINATION" position="0 1.8 0" align="center" color="#00FF00" look-at="[camera]"></a-text>
+            <a-text value="YOU HAVE ARRIVED YOUR DESTINATION" position="0 1.8 0" align="center" color="#fffb00" look-at="[camera]"></a-text>
             <a-entity geometry="primitive: sphere" material="color: gold" animation="property: scale; to: 1.2 1.2 1.2; dir: alternate; loop: true"></a-entity>`;
 
     // NAVIGATION TO LOBBY (If they start elsewhere)
@@ -19,7 +19,7 @@ function loadLevel1() {
         marker1Content = `
     <!-- Lifted 1.5 meters high (Y-axis) -->
     <a-entity position="0 1.5 0">
-        <a-text value="YOU ARE AT: LOBBY" align="center" color="#00FF00" scale="2 2 2" look-at="[camera]"></a-text>
+        <a-text value="YOU ARE AT LOBBY" align="center" color="#fffb00" scale="2 2 2" look-at="[camera]"></a-text>
     </a-entity>
     
     <!-- A large gold ball to make it obvious -->
